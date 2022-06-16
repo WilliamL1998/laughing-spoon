9f68c50bdc69248f8a41cd99fafc4dada679a525
WilliamL1998 <williamliao1998@gmail.com>
2022-06-16 12:45:12 -0400

    Updated server.js

--- server.js
+++ server.js
@@ -1,23 +1,56 @@
 const fs = require('fs');
-const express = require('express');
-const db = require('./db/db.json');
 const path = require('path');
+const express = require('express');
+const uuid = require('uuid');
+const db = require('./db/db.json');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
 })
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
 })
+
+app.get("/api/notes", (req, res) => {
+    return res.sendFile(path.join(__dirname, "db/db.json"));
+})
+
+app.post("/api/notes", (req, res) => {
+    const { title, text } = req.body
+    const newNote = {
+        title,
+        text,
+        id: uuid.v4()
+    }
+    fs.readFile("./db/db.json", "utf-8", (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedNotes = JSON.parse(data);
+            parsedNotes.push(newNote);
+            fs.writeFile(
+                "./db/db.json",
+                JSON.stringify(parsedNotes, null, 4),
+                (writeErr) =>
+                  writeErr
+                    ? console.error(writeErr)
+                    : console.info("Successfully saved note!")
+              );
+            res.sendFile(path.join(__dirname, "public/notes.html"));
+        }
+    })
+})
+
 
 app.listen(PORT, () => {
-    console.log(`Note taker listening at http://localhost:${PORT}`);
-});
+    console.log(`listening at http://localhost:${PORT}`);
+})
