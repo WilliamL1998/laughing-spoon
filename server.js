d69178df3ecc66e70ead84f2669a41afc2040ad8
WilliamL1998 <williamliao1998@gmail.com>
2022-06-16 14:40:24 -0400

    Updated server.js and db.json

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get("/notes", (req, res) => {
 })
 
 app.get("/api/notes", (req, res) => {
-    return res.sendFile(path.join(__dirname, "db/db.json"));
+    res.sendFile(path.join(__dirname, "db/db.json"));
 })
 
 app.post("/api/notes", (req, res) => {
@@ -49,6 +49,9 @@ app.post("/api/notes", (req, res) => {
         }
     })
 })
+
+
+
 
 
 app.listen(PORT, () => {
